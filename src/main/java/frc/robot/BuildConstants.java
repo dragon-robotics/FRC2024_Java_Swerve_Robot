9b439ad43fa7f20d74ed3eca5774ae9094d8773b
Doug Liu <dougdliu@gmail.com>
2024-01-12 01:48:24 -0700

    Added Phoenix 5 again

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024_Java_Swerve_Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "0236f9ff6f739571f7a16a711cfdd5b411ccabfe";
-  public static final String GIT_DATE = "2024-01-12 01:35:38 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "33406ba0258b082e31b04f05eaa2621fa1dbc34b";
+  public static final String GIT_DATE = "2024-01-12 01:38:39 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-12 01:36:09 EST";
-  public static final long BUILD_UNIX_TIME = 1705041369523L;
+  public static final String BUILD_DATE = "2024-01-12 02:37:43 EST";
+  public static final long BUILD_UNIX_TIME = 1705045063200L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
