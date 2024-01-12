33406ba0258b082e31b04f05eaa2621fa1dbc34b
Doug Liu <dougdliu@gmail.com>
2024-01-11 23:38:39 -0700

    1. Updated ReduxLib

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024_Java_Swerve_Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "c4764a4eacc8d18af3c0da28413f2473e9d4b52b";
-  public static final String GIT_DATE = "2024-01-10 03:53:36 EST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "0236f9ff6f739571f7a16a711cfdd5b411ccabfe";
+  public static final String GIT_DATE = "2024-01-12 01:35:38 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-11 16:22:24 EST";
-  public static final long BUILD_UNIX_TIME = 1705008144757L;
+  public static final String BUILD_DATE = "2024-01-12 01:36:09 EST";
+  public static final long BUILD_UNIX_TIME = 1705041369523L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
