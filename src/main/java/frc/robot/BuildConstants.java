43a39ead2ee6f3269f885d306cb8464e805d9f84
Doug <dougdliu@gmail.com>
2024-01-12 15:52:38 -0700

    1. Modified some simulation gui stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2024_Java_Swerve_Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "c4764a4eacc8d18af3c0da28413f2473e9d4b52b";
-  public static final String GIT_DATE = "2024-01-10 03:53:36 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "4178a19b4a8434cfde55c95743f940635688dfe7";
+  public static final String GIT_DATE = "2024-01-11 16:23:33 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-11 16:22:24 EST";
-  public static final long BUILD_UNIX_TIME = 1705008144757L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-01-11 16:46:28 EST";
+  public static final long BUILD_UNIX_TIME = 1705009588507L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
