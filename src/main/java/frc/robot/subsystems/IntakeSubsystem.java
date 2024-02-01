f09f31bd471d27f3861e9365178323975f876de9
Doug Liu <dougdliu@gmail.com>
2024-01-31 23:58:13 -0700

    1. Fixed a comment on the intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -26,7 +26,7 @@ public class IntakeSubsystem extends SubsystemBase {
     // Set the motor to only use 8 volts of power //
     m_intake.enableVoltageCompensation(IntakeConstants.NOMINAL_VOLTAGE);
 
-    // Set the motor to only use 20 amp stall limit, 10 free limit, and limit to 3k RPM //
+    // Set the motor to only use 20 amp stall limit, 10 amp free limit, and limit to 3k RPM //
     m_intake.setSmartCurrentLimit(
         IntakeConstants.STALL_CURRENT_LIMIT,
         IntakeConstants.FREE_CURRENT_LIMIT,
