0c8de7e7cbebdbc88198f9861f5a892e99d1f67b
Doug <dougdliu@gmail.com>
2024-02-23 00:15:06 -0700

    1. Added the IntakeNote command

---
+++ src/main/java/frc/robot/commands/Teleop/IntakeNote.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Teleop;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.UptakeSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class IntakeNote extends ParallelDeadlineGroup {
+  /** Creates a new IntakeNote. */
+  public IntakeNote(IntakeSubsystem intake, UptakeSubsystem uptake, DoubleSupplier speed) {
+    // Add the deadline command in the super() call. Add other commands using
+    // addCommands().
+    super(new MoveUptake(uptake, speed));
+    addCommands(new MoveIntake(intake, speed));
+    // addCommands(new FooCommand(), new BarCommand());
+  }
+}
